127201a40025a8596ea61e590e63082cc16452ab
Linus Romland <romlandlinus@gmail.com>
2021-09-23 09:07:24 +0200

    Uppgift: Användarnamn och lösenord

---
+++ if och else, samt switch/Användarnamn och lösenord/index.js
@@ -0,0 +1,4 @@
+const username = prompt("Username please")
+const password = prompt("Password please")
+
+alert(username === "alice" && password === "secret" ? "Inloggad!" : "Fel användarnamn eller lösenord!")
