77b3740fe3df8b4363a436de5d0f6759122d54c4
Linus Romland <romlandlinus@gmail.com>
2021-10-14 10:27:52 +0200

    Uppgift: Ett sammansatt värde

---
+++ Web Storage/Ett sammansatt värde/index.js
@@ -0,0 +1,13 @@
+let object = [
+    {
+        name: "Alice"
+    },
+    {
+        name: "https://marksism.space is trash"
+    },
+    {
+        name: "https://romland.space is gold"
+    },
+]
+
+sessionStorage.setItem('users', JSON.stringify(object));
