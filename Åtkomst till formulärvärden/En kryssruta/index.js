755a2e40530abf2870d4b766d4d8ed43ffd24197
Linus Romland <romlandlinus@gmail.com>
2021-10-14 10:09:38 +0200

    Uppgift: En kryssruta

---
+++ Åtkomst till formulärvärden/En kryssruta/index.js
@@ -0,0 +1,43 @@
+let button = document.getElementById("send")
+user = document.getElementById("name")
+consent = document.getElementById("consent")
+greeting = document.getElementById("greeting")
+error = document.getElementById("errors")
+userError = document.getElementById("name-error")
+consentError = document.getElementById("consent-error")
+
+if(user.value.length > 0 && consent.checked == true) {
+greeting.innerText = `Hej ${user.value}!`
+button.disabled = false
+} else {
+error.style.display = "block"
+button.disabled = true
+}
+
+button.addEventListener('click', () =>{
+greeting.innerText = `Hej ${user.value}!`
+})
+
+user.addEventListener('input', () =>{
+errorCheck(user.value, consent.checked)
+})
+
+consent.addEventListener('input', () =>{
+errorCheck(user.value, consent.checked)
+})
+
+errorCheck = (textIn, consentIn) => {
+let validText = (textIn.length > 0)
+console.log(validText)
+if(validText && consentIn) {
+    button.disabled = false;
+    error.style.display = "none"
+    userError.hidden = false;
+    consentError.hidden = false;
+} else {
+    button.disabled = true;
+    error.style.display = "block"
+    userError.hidden = (!validText) ? false : true
+    consentError.hidden = (!consentIn) ? false : true
+}
+}
